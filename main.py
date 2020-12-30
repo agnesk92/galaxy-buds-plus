80225fe3035465e0c9cbbfc70dbea906fbf5081b
Agnes Kis <agi.uqbar@gmail.com>
2020-12-30 18:04:02 +0100

    cosmetics

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from battery import get_battery_levels
 from devices import DeviceInfo
 
 
+def get_earbud_battery_info(): 
+    approx_name = "Galaxy Buds"
+
+    device_man = DeviceInfo()
+    device_name = device_man.get_similar_device_names(approx_name)[0]
+    device_address = device_man.get_device_address(device_name)
+
+    return get_battery_levels(device_address) 
+    
+
 class SystemTrayIcon(QSystemTrayIcon):
 
     def __init__(self, icon, parent=None, app=None): 
@@ -17,17 +27,19 @@ class SystemTrayIcon(QSystemTrayIcon):
         self.app = app
 
         reloadAction = menu.addAction("Reload..")
-        reloadAction.triggered.connect(self.reload_tooltip)
+        reloadAction.triggered.connect(self.load_tooltip)
         
         exitAction = menu.addAction("Exit")
         exitAction.triggered.connect(self.close)
         
         self.setContextMenu(menu)
 
-    def reload_tooltip(self):
+    def load_tooltip(self):
         print("Getting battery levels..")    
-        b_left, b_right, b_case = get_earbud_battery_info() 
+        b_left, b_right, b_case = get_earbud_battery_info()
         tooltip_battery = f"Battery {b_left}%, {b_right}%, {b_case}%"
+        
+        print(f"Set tooltip with battery levels: {b_left}%, {b_right}%, {b_case}%")    
         self.setToolTip(tooltip_battery)
 
     def close(self):
@@ -35,26 +47,12 @@ class SystemTrayIcon(QSystemTrayIcon):
         self.app.quit()
         
 
-def get_earbud_battery_info(): 
-    approx_name = "Galaxy Buds"
-    device_man = DeviceInfo()
-    device_name = device_man.get_similar_device_names(approx_name)[0]
-    device_address = device_man.get_device_address(device_name)
-    return get_battery_levels(device_address) 
-    
 def main():
-    print("Create app")
     app = QApplication(sys.argv)
     buds_widget = QWidget()
 
-    print("Creating tray icon..")
     trayIcon = SystemTrayIcon(QtGui.QIcon("galaxy-icon.png"), buds_widget, app=app)
-
-    print("Getting battery levels..")    
-    b_left, b_right, b_case = get_earbud_battery_info() 
-
-    tooltip_battery = f"Battery {b_left}%, {b_right}%, {b_case}%"
-    trayIcon.setToolTip(tooltip_battery)
+    trayIcon.load_tooltip()
     trayIcon.toolTip()
     trayIcon.show()
 
@@ -63,4 +61,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
