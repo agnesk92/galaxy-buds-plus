7583b6893913891db7e1326ec32a894f8438112e
Agnes Kis <agi.uqbar@gmail.com>
2020-12-31 10:56:52 +0100

    add editorconfig

--- app/battery.py
+++ app/battery.py
@@ -8,29 +8,28 @@ config_logger()
 logger = logging.getLogger(__name__)
 
 
-
-def get_battery_levels(bltooth_address): 
+def get_battery_levels(bltooth_address):
     uuid = "00001101-0000-1000-8000-00805F9B34FB"
     service_matches = bluetooth.find_service(uuid=uuid, address=bltooth_address)
 
     host = service_matches[0]["host"]
     port = service_matches[0]["port"]
-    
+
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((host, port))
-    
+
     data = sock.recv(1024)
     batterylife = None
     while data[3] not in [96, 97] and len(data) != 0:
         logger.info(f"Data battery elem: {data[3]}")
-        
+
         data = sock.recv(1024)
         logger.info(len(data))
         if data[3] == 97:
             batterylife = (data[6], data[7], data[11])
         if data[3] == 96:
             batterylife = (data[5], data[6], data[10])
-        
+
     if batterylife is None:
         raise Exception("Couldn't get battery info")
 
@@ -48,16 +47,16 @@ if __name__ == "__main__":
     port = service_matches[0]["port"]
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((host, port))
-    
+
     data = sock.recv(1024)
     batterylife = None
     while data[3] not in [96, 97] and len(data) != 0:
         logger.info(f"Data battery elem: {data[3]}")
-        
+
         data = sock.recv(1024)
         if data[3] == 97:
             batterylife = "{},{},{}".format(data[6], data[7], data[11])
         if data[3] == 96:
             batterylife = "{},{},{}".format(data[5], data[6], data[10])
-        
+
         logger.info(f"Batterylife: {str(batterylife)}")
